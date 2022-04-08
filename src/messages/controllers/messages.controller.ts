e59c33be39d905d189f6c2aad4c0015f99058620
Ewerton Henrique Marschalk <you@example.com>
2022-04-07 21:11:43 -0300

    init repo

---
+++ src/messages/controllers/messages.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
+import { CreateMessageDto } from '../dtos/create-message.dto';
+import { MessagesService } from '../services/messages.service';
+
+@Controller('/messages')
+export class MessagesController {
+  
+  constructor(public messagesService: MessagesService){}
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Get('/:id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+
+    if (!message) {
+      throw new NotFoundException('message not found!');
+    }
+
+    return message;
+
+  }
+
+  @Post()
+  createMessages(@Body() body: CreateMessageDto) {
+    return this.messagesService.create(body.content);
+  }
+}
